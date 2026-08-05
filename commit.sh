eb3562a14eacf3511022ed0ad69f1cdffc3becd8
Unusual Norm <40722525+UnusualNorm@users.noreply.github.com>
2026-08-04 23:36:02 -0500

    2026-08-05T04:36:02Z

---
+++ commit.sh
@@ -0,0 +1,1 @@
+git commit -m "$(date -u +'%Y-%m-%dT%H:%M:%SZ')"
--- profiles/default.nix
+++ profiles/default.nix
@@ -24,6 +24,7 @@
   swapDevices = [ { label = "swap"; } ];
   system.autoUpgrade = {
     enable = true;
+    flags = [ "--no-write-lock-file" ];
     flake = "github:UnusualNorm/nixos-config#${config.networking.hostName}";
   };
   time.timeZone = "America/Chicago";
