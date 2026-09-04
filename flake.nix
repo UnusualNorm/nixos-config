f97b3b34b76cdd422e388b718656b2aafd46d304
Unusual Norm <40722525+UnusualNorm@users.noreply.github.com>
2026-09-03 23:22:57 -0500

    2026-09-04T04:22:57Z

--- flake.nix
+++ flake.nix
@@ -7,8 +7,10 @@
     };
   };
   outputs = inputs@{ home-manager, nixpkgs, self, ... }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem { modules = [ ./nixos.nix home-manager.nixosModules.home-manager ]; };
-    nixosConfigurations.norman = nixpkgs.lib.nixosSystem { modules = [ ./norman.nix home-manager.nixosModules.home-manager ]; };
-    nixosConfigurations.normette = nixpkgs.lib.nixosSystem { modules = [ ./normette.nix home-manager.nixosModules.home-manager ]; };
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem { modules = [ ./nixos.nix home-manager.nixosModules.home-manager ]; };
+      norman = nixpkgs.lib.nixosSystem { modules = [ ./norman.nix home-manager.nixosModules.home-manager ]; };
+      normette = nixpkgs.lib.nixosSystem { modules = [ ./normette.nix home-manager.nixosModules.home-manager ]; };
+    };
   };
 }
